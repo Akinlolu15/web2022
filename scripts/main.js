4f4714810e47d68757c2b7197867e7e6cf4595e8
Oluwatumilara Areola <Oluwatumilara.Areola@uky.edu>
2022-04-24 12:04:32 -0400

    added JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 if (new Date().getHours() > 18) {
   const body = document.querySelector('body');
-  body.style.backgroundColor = '#254c73';
+  body.style.backgroundColor = 'black';
   body.style.color = 'white';
+}
+
+if (new Date().getHours() > 18) {
+  const header = document.querySelector('body');
+  header.style.backgroundColor = 'black';
+  header.style.color = 'white';
 }
